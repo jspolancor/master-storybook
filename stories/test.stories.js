446806d2ec014890dec1e779dd07a1d70c9085e4
MMBA\jpolanco <juan.polanco@mediamonks.com>
2018-08-31 16:05:09 -0300

    TextInfinite submodule

--- stories/test.stories.js
+++ stories/test.stories.js
@@ -1,17 +1,32 @@
 import Vue from 'vue';
 import { storiesOf } from '@storybook/vue';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text, number } from '@storybook/addon-knobs';
 import { withNotes } from '@storybook/addon-notes';
 import Centered from '@storybook/addon-centered';
+import TextInfinite from '../src/component/TextInfinite';
 
 storiesOf('Tests', module)
   .addDecorator(withKnobs)
   .addDecorator(withNotes)
   .addDecorator(Centered)
   .add('Test', () => ({
-    template: `<a @click="log">${text("label", "label")}</a>`,
-    methods: {
-      log: action('click'),
-    }
+    data: () => ({
+      textToShow: text('Text to show', 'Lorem ipsum dolor sit amet'),
+      acceleration: number('Acceleration', 0),
+      padding: number('Padding', 1),
+      speed: number('Speed', 1),
+    }),
+    components: {
+      TextInfinite,
+    },
+    template:
+      `<div style="overflow: hidden; width: 100vw;">
+        <text-infinite
+            :text="textToShow"
+            :speed="speed"
+            :acceleration="acceleration"
+            :padding="padding">
+        </text-infinite>
+       </div>`,
   }), {notes: 'This is a note'});
